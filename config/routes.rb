5033319bec6e2728cab3b2b2dc7c8cc95c251a48
Philip Medlin <phmedlin@gmail.com>
2021-10-12 15:09:13 -0600

    Feat: Add iteration 1 controllers, views, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to:'welcome#index'
+
+  get '/restaurant', to: 'restaurant#index'
+
+  get '/brewery', to: 'brewery#index'
+
 end
