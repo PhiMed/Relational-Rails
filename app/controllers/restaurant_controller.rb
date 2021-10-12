077f046c3711a4747ea0a95bef2d44b977d17834
Didi Dodson <mdodson55@gmail.com>
2021-10-12 16:05:08 -0600

    Feat: add new and create methods

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -1,6 +1,23 @@
 class RestaurantController < ApplicationController
 
   def index
-    @restaurant = ["Arvada Tavern", "Chez Gustaf", "Los Gatos"]
+    @restaurant = Restaurant.all
+  end
+
+  def new
+  end
+
+  def create
+    restaurant = Restaurant.new({
+      name: params[:restaurant][:name],
+      created_at: params[:restaurant][:created_at],
+      updated_at: params[:restaurant][:updated_at],
+      seats: params[:restaurant][:seats],
+      serve_liquor: params[:restaurant][:serve_liquor]
+      })
+
+    restaurant.save
+
+    redirect_to '/restaurant'
   end
 end
