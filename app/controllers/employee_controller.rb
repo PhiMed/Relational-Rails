46a09c1c8641ff3d2e57ad4532c38e3cf730a43c
Didi Dodson <mdodson55@gmail.com>
2021-10-13 16:22:41 -0600

    Add employee show functionality

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,5 +1,28 @@
 class EmployeeController < ApplicationController
+
   def index
     @employee = Employee.all
+  end
+
+  def new
+  end
+
+  def create
+    employee = Employee.new({
+      name: params[:employee][:name],
+      weekly_hours: params[:employee][:weekly_hours],
+      over_21: params[:employee][:over_21],
+      created_at: params[:employee][:created_at],
+      updated_at: params[:employee][:updated_at],
+      restaurant_id: params[:employee][:restaurant_id]
+      })
+
+    employee.save
+
+    redirect_to '/employee'
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
   end
 end
