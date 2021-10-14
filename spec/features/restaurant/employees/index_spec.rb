a862566603e89a3d97947480f72ae044ca91b096
Didi Dodson <mdodson55@gmail.com>
2021-10-13 21:08:06 -0600

    Update spec features for restaurant

---
+++ spec/features/restaurant/employees/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'restaurant employee index' do
+  it 'shows all employees of restaurants' do
+    restaurant = Restaurant.create!(name: "Taco Bell",
+                                    seats: 12,
+                                    serve_liquor: false)
+
+    employee = restaurant.employees.create(name: "Bob Smith",
+                                            weekly_hours: 40,
+                                            over_21: true,
+                                            restaurant_id: restaurant.id)
+
+    visit "/restaurant/#{restaurant.id}/employee"
+
+    expect(page).to have_content(employee.name)
+    expect(page).to have_content(employee.weekly_hours)
+  end
+end
