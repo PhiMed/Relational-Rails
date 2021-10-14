cc90559d20196b894f51c05c09445ae2c20901cf
Philip Medlin <phmedlin@gmail.com>
2021-10-14 13:57:22 -0600

    Test: Models and features tests

---
+++ spec/features/beer/beer_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe "beer", type: :feature do
+  it 'displays index' do
+    brewery = Brewery.create!(name: "Breckenridge Brewery",
+                                    number_of_employees: 120,
+                                    employee_owned: false)
+
+    beer = Beer.create!(name: "Autumn Haze IPA",
+                                vendor_lead_time: 7,
+                                bottled: true,
+                                brewery_id: brewery.id)
+
+    visit "/beer"
+
+    expect(page).to have_content(beer.name)
+    expect(page).to_not have_content("Pumpkin Spice Latte")
+  end
+
+  it 'displays beer attributes' do
+    brewery = Brewery.create!(name: "Breckenridge Brewery",
+                                    number_of_employees: 120,
+                                    employee_owned: false)
+
+    beer = Beer.create!(name: "Autumn Haze IPA",
+                                vendor_lead_time: 7,
+                                bottled: true,
+                                brewery_id: brewery.id)
+
+
+    visit "/beer/#{beer.id}"
+
+    expect(page).to have_content(beer.id)
+    expect(page).to have_content(beer.brewery_id)
+    expect(page).to_not have_content("Pumpkin Spice Latte")
+  end
+end
