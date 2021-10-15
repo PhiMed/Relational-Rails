30c685c474c02ce678bd5ae51b1523713a20a332
Didi Dodson <mdodson55@gmail.com>
2021-10-15 16:28:11 -0600

    Updatte restaurant spec

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
   it {should have_many :employees}
+
+  it 'is sorted by created_at date' do
+    restaurant_1 = Restaurant.create!(name: "Taco Bell",
+                                    seats: 12,
+                                    serve_liquor: false)
+    restaurant_2 = Restaurant.create!(name: "Del Taco",
+                                      seats:25,
+                                      serve_liquor: false)
+
+    expect(Restaurant.all.order_desc.first).to eq(restaurant_2)
+    expect(Restaurant.all.order_desc.first).to_not eq(restaurant_1)
+  end  
 end
