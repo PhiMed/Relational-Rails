04676bcffac0e4a44cc846f6b6ac86ea7bfcdedd
Didi Dodson <mdodson55@gmail.com>
2021-10-16 10:00:14 -0600

    Add User story 17

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,7 +1,7 @@
 class EmployeeController < ApplicationController
 
   def index
-    @employee = Employee.all
+    @employee = Employee.all.true_records
   end
 
   def new
@@ -20,5 +20,23 @@ class EmployeeController < ApplicationController
 
   def show
     @employee = Employee.find(params[:id])
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+      employee = Employee.find(params[:id])
+      employee.update({
+        name: params[:employee][:name],
+        weekly_hours: params[:employee][:weekly_hours],
+        over_21: params[:employee][:over_21],
+        restaurant_id: params[:employee][:restaurant_id]
+        })
+
+      employee.save
+
+    redirect_to "/employee/#{employee.id}"
   end
 end
--- spec/features/restaurant/index_spec.rb
+++ spec/features/restaurant/index_spec.rb
@@ -14,9 +14,19 @@ RSpec.describe "restaurant", type: :feature do
     expect(page).to have_content(restaurant.name)
     expect(page).to have_content(restaurant_2.name)
     expect(page).to_not have_content("Del Taco")
+
+    click_link "#{restaurant.name}"
+    expect(current_path).to eq("/restaurant/#{restaurant.id}")
   end
 
-  it 'is sorted by created_at date' do
+  it 'has link to add new restaurant' do
+    visit "/restaurant"
+
+    click_link "New Restaurant"
+    expect(current_path).to eq('/restaurant/new')
+  end
+
+  it 'is sorted by created_at date and shows that date' do
     restaurant_1 = Restaurant.create!(name: "Taco Bell",
                                     seats: 12,
                                     serve_liquor: false)
@@ -24,18 +34,39 @@ RSpec.describe "restaurant", type: :feature do
                                       seats:25,
                                       serve_liquor: false)
 
+    visit "/restaurant"
+
     expect(Restaurant.all.order_desc.first).to eq(restaurant_2)
     expect(Restaurant.all.order_desc.first).to_not eq(restaurant_1)
+    expect(page).to have_content(restaurant_1.created_at)
+    expect(page).to have_content(restaurant_2.created_at)
   end
 
-  it 'provides links for employee and restaurant indexes' do
-    restaurant = Restaurant.create!(name: "Taco Bell",
-                                    seats: 12,
-                                    serve_liquor: false)
-
+  it 'provides links for employee, restaurant, welcome indexes' do
     visit "/restaurant"
 
     expect(page).to have_link("Employee Index")
     expect(page).to have_link("Restaurant Index")
+    expect(page).to have_link("Welcome Index")
   end
+
+  it 'provides update link for each restaurant' do
+    restaurant_1 = Restaurant.create!(name: "Taco Bell",
+                                    seats: 12,
+                                    serve_liquor: false)
+
+    visit "/restaurant"
+    expect(page).to have_link("Update Restaurant")
+
+    click_link "Update Restaurant"
+    expect(current_path).to eq("/restaurant/#{restaurant_1.id}/edit")
+
+    fill_in "restaurant[name]", with: "Del Taco"
+    fill_in "restaurant[seats]", with: "15"
+    fill_in "restaurant[serve_liquor]", with: "true"
+    click_button "Update Restaurant"
+
+    expect(current_path).to eq("/restaurant/#{restaurant_1.id}")
+  end
+
 end
